7a66b9810b157416b3952bc91b4ad947440147f3
Coders9614 <joonyong.kim1@outlook.com>
2024-06-06 16:31:09 -0600

    Add project files.

---
+++ OverloadCircle/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Circle
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Overload the + operator to calculate the sum of areas
+        public static double operator +(Circle circle1, Circle circle2)
+        {
+            return Math.PI * (circle1.Radius * circle1.Radius + circle2.Radius * circle2.Radius);
+        }
+
+        // Overload the - operator to calculate the area difference
+        public static double operator -(Circle circle1, Circle circle2)
+        {
+            return Math.Abs(Math.PI * (circle1.Radius * circle1.Radius - circle2.Radius * circle2.Radius));
+        }
+    }
+
---
+++ OverloadCircle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadCircle
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius1 = 5.0;
+            double radius2 = 3.0;
+
+            Circle circle1 = new Circle(radius1);
+            Circle circle2 = new Circle(radius2);
+
+            double sumOfAreas = circle1 + circle2;
+            double areaDifference = circle1 - circle2;
+
+            Console.WriteLine($"Sum of areas: {sumOfAreas}");
+            Console.WriteLine($"Area difference: {areaDifference}");
+            Console.ReadKey();
+        }
+    }
+}
